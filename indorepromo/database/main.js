9c193927e0de9ea0e1fa1c95d798a0ede594f4b2
livewebhost <sandeepkashyap.marketing@gmail.com>
2025-10-19 14:26:08 +0530

    ok

--- indorepromo/database/main.js
+++ indorepromo/database/main.js
@@ -8,34 +8,3 @@
   document.getElementById("profile-image").src = database.profile.about.profileimage;
   document.getElementById("cover-image").src = database.profile.about.coverimage;
   document.getElementById("my-qr-code-url").src = database.site.qrurl;
-
-
-  function downloadImage() {
-    const imagePath = database.site.qrurl;
-    const fileName = 'qrcode.png';
-    const link = document.createElement('a');
-    link.href = imagePath;
-    link.download = fileName;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-  /*------------------------------------- Share Scanner Img -------------------------------------*/
-  function shareImage() {
-    const imageUrl = database.site.qrurl;
-    const title = 'Share Scanner Image';
-    const text = 'Check out this image!';
-    if (navigator.share) {
-      navigator.share({
-        title: title,
-        text: text,
-        url: imageUrl
-      }).then(() => {
-        console.log('Image shared successfully.');
-      }).catch((error) => {
-        console.error('Error sharing image:', error);
-      });
-    } else {
-      alert('Sharing is not supported on this device.');
-    }
-  }
